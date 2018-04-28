8f8630bb20c1bfc420c1b4585f1600afb965bf8d
liangbo <821489625@qq.com>
2018-04-28 09:51:50 +0800

    添加jar启动加载

--- chilopod-slave/src/main/java/cn/dtvalley/chilopod/slave/TaskManager.java
+++ chilopod-slave/src/main/java/cn/dtvalley/chilopod/slave/TaskManager.java
@@ -4,8 +4,17 @@ import cn.dtvalley.chilopod.core.instance.SlaveTask;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 
 public final class TaskManager {
+    private TaskManager(){
+
+    }
+
+    public  static  final ExecutorService threadPoolExecutor = Executors.newCachedThreadPool();
+
     private static final Map<String, SlaveTask> tasks = new ConcurrentHashMap<>();
 
     public static Map<String, SlaveTask> getTasks() {
